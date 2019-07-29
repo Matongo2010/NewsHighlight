7ae0fcf15ccc6fa142fb02a9aae198a5bd67003b
Matongo2010 <justineadriano638@gmail.com>
2019-07-29 08:27:48 +0300

    created a main route in the view.py file

--- manage.py
+++ manage.py
@@ -1,11 +1,19 @@
-from flask import Flask,render_template
+from app import create_app
+from flask_script import Manager,Server
 
-app = Flask(__name__)
+# Creating app instance
+app = create_app('development')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
-    
-    app.run(debug=True) 
+    manager.run()
--- static/views.py
+++
@@ -1,4 +0,0 @@
-from flask import render_template,request,redirect,url_for
-from . import main
-from ..requests import get_sources,get_articles
-from ..models import Sources
